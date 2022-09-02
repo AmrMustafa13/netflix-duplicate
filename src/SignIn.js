20b2c35cecaa856bce1a63fed827a95ea524aef2
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-09-02 21:12:07 +0200

    sign-in-ui-components

--- src/SignIn.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  return <div>This issss test</div>;
-};
-
-export default SignIn;
---
+++ src/modules/auth/SignIn/SignIn.js
@@ -0,0 +1,69 @@
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import classes from "./SignIn.module.css";
+import NetflixLogo from "../../../assets/netflix_logo.png";
+
+const SignIn = () => {
+  return (
+    <div className={classes["sign-in"]}>
+      <Stack zIndex={1} position="absolute" top={0} left={0} right={0} p="24px">
+        <img
+          src={NetflixLogo}
+          alt="Netflix-Logo"
+          width={200}
+          style={{ cursor: "pointer" }}
+        />
+      </Stack>
+      <form>
+        <Typography variant="h4" sx={{ color: "white", fontWeight: "bold" }}>
+          Sign In
+        </Typography>
+        <TextField
+          label="Email or phone number"
+          variant="outlined"
+          type="email"
+          sx={{ width: "350px" }}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          sx={{ width: "350px" }}
+        />
+        <Button
+          variant="contained"
+          color="error"
+          fullWidth
+          sx={{ padding: "14px" }}
+        >
+          Sign In
+        </Button>
+        <Stack
+          justifyContent="space-between"
+          direction="row"
+          alignItems="center"
+        >
+          <FormControlLabel label="Remember me" control={<Checkbox />} />
+          <span>Need help?</span>
+        </Stack>
+        <Stack
+          justifyContent="space-between"
+          direction="row"
+          alignItems="center"
+        >
+          <span>New to Netflix?</span>
+          <a href="/signup">Sign up now</a>
+        </Stack>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
